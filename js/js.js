c68c1b43b0911d984ce1041f3f59b78e096e64a9
Lazizjonov <44605450+Lazizjonov@users.noreply.github.com>
2020-10-24 17:48:56 +0500

    mobile js and some css

--- js/js.js
+++ js/js.js
@@ -37,10 +37,10 @@ function sbmt(inputdata, url, func_name) {
     }
 }
 
-var scrollLock = false;
+var scrollLock = true;
 var scrollEventCatch = (event)=>{
     // console.log(event);
-    if(!scrollLock){
+    if(scrollLock){
         if(event.deltaY > 0){
             // pasga
             activeOne = dqs("section.block.active");
@@ -68,12 +68,12 @@ var scrollEventCatch = (event)=>{
             dqs(".logo img").src = dqs(".logo img").dataset.white;
             dqs(".menu").classList.remove("black");
         }
-    }
-    
-    scrollLock = true;
-    setTimeout(()=>{
+        
         scrollLock = false;
-    }, 1000);
+        setTimeout(()=>{
+            scrollLock = true;
+        }, 1000);
+    }
 }
 
 var blocks = dqs("section.block");
@@ -108,3 +108,44 @@ if(burger){
         dqs(".menu__hid").classList.toggle("active");
     });
 }
+
+
+var tX = 0;
+var tY = 0;
+function touchControl(event){
+    // console.log(event.touches[0].pageX);
+    // console.log(event.touches[0].pageY);
+    tX = event.changedTouches[0].screenX;
+    tY = event.changedTouches[0].screenY;
+    console.log(event.changedTouches[0].screenY);
+    //console.log(event);
+}
+function touchControlEnd(event){
+    //console.log(event);
+    console.log(event.changedTouches[0].screenY);
+    var ev = {};
+    if(event.changedTouches[0].screenY - tY > 50){
+        //tepaga
+        ev.deltaY = -100;
+        scrollEventCatch(ev);
+    }else if(event.changedTouches[0].screenY - tY < -50){
+        //pasga
+        ev.deltaY = 100;
+        scrollEventCatch(ev);
+    }
+}
+document.addEventListener("touchstart", touchControl);
+document.addEventListener("touchend", touchControlEnd);
+// touches: TouchList
+// 0: Touch
+// clientX: 191
+// clientY: 228
+// force: 1
+// identifier: 0
+// pageX: 191
+// pageY: 228
+// radiusX: 11.5
+// radiusY: 11.5
+// rotationAngle: 0
+// screenX: 951
+// screenY: 366
